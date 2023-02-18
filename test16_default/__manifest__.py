f3ad2d4328352affcbcd19a74cb0aa575cbfe4a7
matias <mmoriena@quilsoft.com>
2023-02-18 07:57:16 -0300

    [ADD] actualizar manifest

--- test16_default/__manifest__.py
+++ test16_default/__manifest__.py
@@ -1,10 +1,3 @@
-##############################################################################
-#
-#    Copyright (C) 2021  Quilsoft  (http://www.quilsoft.com)
-#    All Rights Reserved.
-#
-##############################################################################
-
 {
     "name": "test16",
     "version": "16.0.1.0.0",
@@ -20,23 +13,10 @@
     "env-ver": "2",
     "odoo-license": "CE",
     "config": [
-        # 'addons_path' is always computed looking for the repositories in sources
-        # 'data_dir' is a fixed location inside docker odoo image
-        # You should use 2 worker threads + 1 cron thread per available CPU,
-        # and 1 CPU per 10 concurent users.
-        # if ommited oe will calculate workers and cron´s based on # of cpu
         "workers = 0",
         "max_cron_threads = 1",
-        # Number of requests a worker will process before being recycled and
-        # restarted. Defaults to 8192 if ommited
         "limit_request = 8192",
-        # Maximum allowed virtual memory per worker. If the limit is exceeded,
-        # the worker is killed and recycled at the end of the current request.
-        # Defaults to 640MB
         "limit_memory_soft = 2147483648",
-        # Hard limit on virtual memory, any worker exceeding the limit will be
-        # immediately killed without waiting for the end of the current request
-        # processing. Defaults to 768MB.
         "limit_memory_hard = 2684354560",
     ],
     "port": "8069",
